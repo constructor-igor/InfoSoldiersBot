b308c333f6bc9215ebea2c279ed368bd9e838656
Igor Ziselman <igor.zc@gmail.com>
2023-11-01 20:05:30 +0200

    Update InfoSoldiers.py

--- src/InfoSoldiers.py
+++ src/InfoSoldiers.py
@@ -53,8 +53,7 @@ def start_bot():
 
     all_items = messages_builder.import_scheduler()
     for hour, minutes, message_file_name in all_items:
-        cloned_message_file_name = message_file_name[:]
-        message_func = lambda:messages_builder.get_message(cloned_message_file_name)
+        message_func = lambda m=message_file_name: messages_builder.get_message(m)
         scheduler_message.add_event(hour=hour, minutes=minutes, message_func=message_func)
     scheduler_message.add_event(hour=9, minutes=0, message_func=messages_builder.get_daily_message)
     # scheduler_message.add_polling(custom_polling_func=red_alert_checking, sleep_seconds=1)
